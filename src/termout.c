9ba3d4891965b49e9d95c7dc782e2e025bbad3c9
his-ishi <Hisashi.Ishiwata@gmail.com>
2017-01-28 00:36:29 +0900

    fix indent

--- src/termout.c
+++ src/termout.c
@@ -1637,9 +1637,9 @@ term_write(const char *buf, uint len)
           uint i = term.csi_argc - 1;
           if (i < lengthof(term.csi_argv))
             term.csi_argv[i] = 10 * term.csi_argv[i] + c - '0';
-            if ((int)term.csi_argv[i] < 0)
-              term.csi_argv[i] = INT_MAX;  // capture overflow
-            term.csi_argv_defined[i] = 1;
+          if ((int)term.csi_argv[i] < 0)
+            term.csi_argv[i] = INT_MAX;  // capture overflow
+          term.csi_argv_defined[i] = 1;
         }
         else if (c < 0x40)
           term.esc_mod = term.esc_mod ? 0xFF : c;
